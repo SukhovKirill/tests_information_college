65731d15712687f76fb95bf09e1f2454422b177b
SukhovKirill <160316848+SukhovKirill@users.noreply.github.com>
2024-09-14 11:04:19 +0400

    Add files via upload

---
+++ calculator1.py
@@ -0,0 +1,41 @@
+def addition(a, b):
+    return a + b
+
+def subtraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Ошибка: Деление на ноль"
+
+q1 = int(input('Введите число 1: '))
+q2 = int(input('Введите число 2: '))
+
+v = int(input('Какую операцию вы хотите выполнить? \n 1 Сложение \n 2 Вычитание \n 3 Деление \n 4 Умножение \n'))
+
+if v == 1:
+    r = addition(q1, q2)
+    operation = 'сложения'
+elif v == 2:
+    r = subtraction(q1, q2)
+    operation = 'вычитания'
+elif v == 3:
+    r = division(q1, q2)
+    operation = 'деления'
+elif v == 4:
+    r = multiplication(q1, q2)
+    operation = 'умножения'
+else:
+    r = "Ошибка: Неверный выбор операции"
+    operation = ""
+
+if operation:
+    print('Результат ', operation, ' = ', r)
+else:
+    print(r)
+    
